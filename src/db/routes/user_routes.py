5b555b3ff6fde5b0f16036da5fadaf1d393ba069
Tovar <tovarobe@amazon.es>
2025-04-29 16:43:43 +0200

    Added validators to user_routes

--- src/db/routes/user_routes.py
+++ src/db/routes/user_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify
 from src.db.models.user_models import Users
 from src.db.models.user_follows_models import Follow
 from flask_cors import CORS
-from ...extensions import db, validate
+from ...extensions import db, validate_existence
 from flask_jwt_extended import jwt_required
 
 user_api = Blueprint('user_api', __name__)
@@ -14,30 +14,28 @@ def get_users():
     response_body = {}
     users = Users.query.all()
     results = [user.serialize() for user in users]
-    response_body['message'] = "List of users"
-    response_body['results'] = results
+    response_body["message"] = "List of users"
+    response_body["results"] = results
     return jsonify(response_body), 200
 
 @user_api.route('/users/<int:user_id>', methods=['GET'])
 def get_user(user_id):
     response_body= {}
     user = Users.query.get(user_id)
-
-    if not user:
-        return jsonify({'message': 'Invalid user'}), 404
+    validate_existence(user, "user")
     
-    response_body['message'] = 'User found'
-    response_body['results'] = user.serialize()
+    response_body["message"] = 'User found'
+    response_body["results"] = user.serialize()
     return jsonify(response_body), 200
 
-@user_api.route('/users/<int:user_id>', methods=['PUT'])
+@user_api.route('/users/<int:user_id>', methods=["PUT"])
 def edit_user(user_id):
     response_body = {}
     user = Users.query.get(user_id)
+    validate_existence(user, "user")
     data = request.json 
-    
-    if not user:
-        return jsonify({'message': 'Invalid user'}), 404
+    validate_existence(data, "data")
+
     
     fields = ['email', 'password', 'name', 'last_name', 'username', 'weight']
     changed_fields = []
@@ -48,19 +46,17 @@ def edit_user(user_id):
             changed_fields.append(field)            
 
     db.session.commit()
-    response_body['message'] = "Data updated"
-    response_body['results'] = user.serialize()
-    response_body['changed_fields'] = changed_fields
+    response_body["message"] = "Data updated"
+    response_body["results"] = user.serialize()
+    response_body["changed_fields"] = changed_fields
     return jsonify(response_body), 200
 
 @user_api.route('/users/<int:user_id>', methods=['DELETE'])
 def delete_user(user_id):
     user = Users.query.get(user_id)
+    validate_existence(user, "user")
     response_body = {}
-
-    if not user:
-        return jsonify({'message': 'User not found'}), 404
-    
+  
     Follow.query.filter_by(user_id=user_id).delete()
     Follow.query.filter_by(following_id=user_id).delete()
 
