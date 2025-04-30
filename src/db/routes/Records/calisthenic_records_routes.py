c6bb45309a480c36b5a5993ac518ef4ea860155b
Tovar <tovarobe@amazon.es>
2025-04-30 17:49:30 +0200

    refactored weighted_records and created calisthenic_records

---
+++ src/db/routes/Records/calisthenic_records_routes.py
@@ -0,0 +1,119 @@
+from flask import Blueprint, request, jsonify
+from flask_cors import CORS
+from src.db.models.calisthenic_models import CalisthenicRecord, CalisthenicExerciseVariations
+from ....extensions import db, validate_existence
+from datetime import datetime
+
+calisthenic_records_api = Blueprint("calisthenic_records_api", __name__)
+
+CORS(calisthenic_records_api)
+
+@calisthenic_records_api.route('/users/<int:user_id>/exercises/calisthenic/<int:exercise_id>/variations/<int:variation_id>/records',
+                               methods=["GET"])
+def get_calisthenic_variation_records(user_id, exercise_id, variation_id):
+    variation = CalisthenicExerciseVariations.query.filter_by(
+        id=variation_id,
+        exercise=exercise_id
+    ).first()
+    validate_existence(variation, "variation")
+    
+    records = CalisthenicRecord.query.filter_by(
+        user_id=user_id,
+        variation_id=variation_id
+    ).all()
+    # validate_existence(records, "records")
+
+    all_records = [record.serialize() for record in records]
+
+    return jsonify(all_records), 200
+
+@calisthenic_records_api.route('/users/<int:user_id>/exercises/calisthenic/<int:exercise_id>/variations/<int:variation_id>/records',
+                               methods=["POST"])
+def add_calisthenic_cariation_record(user_id, exercise_id, variation_id):
+    data = request.json
+    validate_existence(data.get("repetitions"), "repetitions")
+
+    variation = CalisthenicExerciseVariations.query.filter_by(
+        id=variation_id,
+        exercise_id=exercise_id
+    ).first()
+    validate_existence(variation, "variation")
+
+    new_record = CalisthenicRecord(
+        repetitions=data["repetitions"],
+        date = datetime.utcnow().date,
+        is_a_challenge=data.get("is_a_challenge", False),
+        user_id=user_id,
+        variation_id=variation_id
+    )
+
+    db.session.add(new_record)
+    db.session.commit()
+
+    return jsonify(new_record.serialize()), 201
+
+@calisthenic_records_api.route('/users/<int:user_id>/exercises/calisthenic/<int:exercise_id>/variations/<int:variation_id>/records/<int:record_id>',
+                               methods=["GET"])
+def get_single_calisthenic_variation_record(user_id, exercise_id, variation_id, record_id):
+    variation = CalisthenicExerciseVariations.query.filter_by(
+        id=variation_id,
+        exercise_id=exercise_id 
+    ).first()
+    validate_existence(variation, "variation")
+
+    record = CalisthenicRecord.query.filter_by(
+        id=record_id,
+        user_id=user_id,
+        variation_id=variation_id
+    ).first()
+    validate_existence(record, "record")
+
+    return jsonify(record.serialize()), 200
+
+@calisthenic_records_api.route('/users/<int:user_id>/exercises/calisthenic/<int:exercise_id>/variations/<int:variation_id>/records/<int:record_id>',
+                               methods=["PUT"])
+def edit_single_calisthenic_variation_record(user_id, exercise_id, variation_id, record_id):
+    variation = CalisthenicExerciseVariations.query.filter_by(
+        id=variation_id,
+        exercise_id=exercise_id
+    ).first()
+    validate_existence(variation, "variation")
+
+    record = CalisthenicRecord.query.filter_by(
+        id=record_id,
+        user_id=user_id,
+        variation_id=variation_id
+    ).first()
+    validate_existence(record, "record")
+
+    data = request.json
+    validate_existence(data, "data")
+
+    record.repetitions = data["repetitions"]
+    if "date" in data:
+        record.date = datetime.strptime(data["date"], "%d/%m/%Y").date()
+
+    db.session.commit()
+
+    return jsonify({'message': 'The record has been edited correctly', 'record': record.serialize()}), 200
+
+@calisthenic_records_api.route('/users/<int:user_id>/exercises/calisthenic/<int:exercise_id>/variations/<int:variation_id>/records/<int:record_id>',
+                               methods=["DELETE"])
+def delete_single_calisthenic_variation_record(user_id, exercise_id, variation_id, record_id):
+    variation = CalisthenicExerciseVariations.query.filter_by(
+        id=variation_id,
+        exercise_id=exercise_id
+    ).first()
+    validate_existence(variation, "variation")
+
+    record = CalisthenicRecord.query.filter_by(
+        id=record_id,
+        user_id=user_id,
+        variation_id=variation_id
+    ).first()
+    validate_existence(record, "record")
+
+    db.session.delete(record)
+    db.session.commit()
+
+    return jsonify({'message':'The record has been deleted successfully'}), 200
--- src/db/routes/Records/weighted_records_routes.py
+++ src/db/routes/Records/weighted_records_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 from flask_cors import CORS
 from src.db.models.weighted_models import WeightRecord, WeightedExerciseVariations
-from ...extensions import db, validate_existence
+from ....extensions import db, validate_existence
 from datetime import datetime
 
 weighted_records_api = Blueprint('weighted_records_api', __name__)
@@ -11,13 +11,17 @@ CORS(weighted_records_api)
 @weighted_records_api.route('/users/<int:user_id>/exercises/weighted/<int:exercise_id>/variations/<int:variation_id>/records',
                             methods=["GET"])
 def get_weighted_variation_records(user_id, exercise_id, variation_id):
-    variation = WeightedExerciseVariations.query.filter_by(id=variation_id, exercise_id=exercise_id).first()
-    validate_existence(variation, "records")
+    variation = WeightedExerciseVariations.query.filter_by(
+        id=variation_id,
+        exercise_id=exercise_id
+    ).first()
+    validate_existence(variation, "variation")
+    
     records = WeightRecord.query.filter_by(
         user_id=user_id,
         variation_id=variation_id
     ).all()
-    validate_existence(records, "records")
+    # validate_existence(records, "records")
 
     all_records = [record.serialize() for record in records]
 
@@ -25,11 +29,14 @@ def get_weighted_variation_records(user_id, exercise_id, variation_id):
 
 @weighted_records_api.route('/users/<int:user_id>/exercises/weighted/<int:exercise_id>/variations/<int:variation_id>/records',
                             methods=["POST"])
-def add_weighted_variation_record(user_id, exercise_id, variation_id):
+def add_weighted_variation_record(user_id, exercise_id, variation_id): 
     data = request.json
     validate_existence(data.get("lifted_weight"), "lifted_weight")
     
-    variation = WeightedExerciseVariations.query.filter_by(id=variation_id, exercise_id=exercise_id).first()
+    variation = WeightedExerciseVariations.query.filter_by(
+        id=variation_id,
+        exercise_id=exercise_id
+    ).first()
     validate_existence(variation, "variation")
     
     new_record = WeightRecord(
@@ -45,14 +52,20 @@ def add_weighted_variation_record(user_id, exercise_id, variation_id):
 
     return jsonify(new_record.serialize()), 201
 
-
 @weighted_records_api.route('/users/<int:user_id>/exercises/weighted/<int:exercise_id>/variations/<int:variation_id>/records/<int:record_id>',
                             methods=["GET"])
 def get_single_weighted_variation_record(user_id, exercise_id, variation_id, record_id):
-    variation = WeightedExerciseVariations.query.filter_by(id=variation_id, exercise_id=exercise_id).first()
+    variation = WeightedExerciseVariations.query.filter_by(
+        id=variation_id,
+        exercise_id=exercise_id
+    ).first()
     validate_existence(variation, "variation")
    
-    record = WeightRecord.query.filter_by(id=record_id, user_id=user_id, variation_id=variation_id).first()
+    record = WeightRecord.query.filter_by(
+        id=record_id,
+        user_id=user_id,
+        variation_id=variation_id
+    ).first()
     validate_existence(record, "record")
 
     return jsonify(record.serialize()), 200
@@ -60,18 +73,25 @@ def get_single_weighted_variation_record(user_id, exercise_id, variation_id, rec
 @weighted_records_api.route('/users/<int:user_id>/exercises/weighted/<int:exercise_id>/variations/<int:variation_id>/records/<int:record_id>',
                             methods=["PUT"])
 def edit_single_weighted_variation_record(user_id, exercise_id, variation_id, record_id):
-    variation = WeightedExerciseVariations.query.filter_by(id=variation_id, exercise_id=exercise_id).first()
+    variation = WeightedExerciseVariations.query.filter_by(
+        id=variation_id,
+        exercise_id=exercise_id
+    ).first()
     validate_existence(variation, "variation")
-    record = WeightRecord.query.filter_by(id=record_id, user_id=user_id, variation_id=variation_id).first()
+
+    record = WeightRecord.query.filter_by(
+        id=record_id,
+        user_id=user_id,
+        variation_id=variation_id
+    ).first()
     validate_existence(record, "record")
     
     data = request.json
     validate_existence(data, "data")
     
-    record.lifted_weight = data.get("lifted_weight", record.lifted_weight)
+    record.lifted_weight = data["repetitions"]
     if "date" in data:
         record.date = datetime.strptime(data["date"], "%d/%m/%Y").date()
-    record.is_a_challenge = record.is_a_challenge
 
     db.session.commit()
 
@@ -80,9 +100,17 @@ def edit_single_weighted_variation_record(user_id, exercise_id, variation_id, re
 @weighted_records_api.route('/users/<int:user_id>/exercises/weighted/<int:exercise_id>/variations/<int:variation_id>/records/<int:record_id>',
                             methods=["DELETE"])
 def delete_single_weighted_variation_record(user_id, exercise_id, variation_id, record_id):
-    variation = WeightedExerciseVariations.query.filter_by(id=variation_id, exercise_id=exercise_id).first()
+    variation = WeightedExerciseVariations.query.filter_by(
+        id=variation_id,
+        exercise_id=exercise_id
+    ).first()
     validate_existence(variation, "variation")
-    record = WeightRecord.query.filter_by(id=record_id, user_id=user_id, variation_id=variation_id).first()
+
+    record = WeightRecord.query.filter_by(
+        id=record_id,
+        user_id=user_id,
+        variation_id=variation_id
+    ).first()
     validate_existence(record, "record")
     
     db.session.delete(record)
