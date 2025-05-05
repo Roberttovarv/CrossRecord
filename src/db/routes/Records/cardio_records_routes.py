db9da554bdbb60ce3a4c405164e52e38eb19e8de
Tovar <tovarobe@amazon.es>
2025-05-05 13:04:30 +0200

    Created cardio records route

--- src/db/routes/Records/cardio_records_routes.py
+++ src/db/routes/Records/cardio_records_routes.py
@@ -1,23 +1,23 @@
 from flask import Blueprint, request, jsonify
 from flask_cors import CORS
-from src.db.models.calisthenic_models import CalisthenicRecord, CalisthenicExerciseVariations
+from src.db.models.cardio_models import CardioRecord, CardioExerciseVariations
 from ....extensions import db, validate_existence
 from datetime import datetime
 
-calisthenic_records_api = Blueprint("calisthenic_records_api", __name__)
+cardio_records_api = Blueprint("cardio_records_api", __name__)
 
-CORS(calisthenic_records_api)
+CORS(cardio_records_api)
 
-@calisthenic_records_api.route('/users/<int:user_id>/exercises/calisthenic/<int:exercise_id>/variations/<int:variation_id>/records',
+@cardio_records_api.route('/users/<int:user_id>/exercises/cardio/<int:exercise_id>/variations/<int:variation_id>/records',
                                methods=["GET"])
-def get_calisthenic_variation_records(user_id, exercise_id, variation_id):
-    variation = CalisthenicExerciseVariations.query.filter_by(
+def get_cardio_variation_records(user_id, exercise_id, variation_id):
+    variation = CardioExerciseVariations.query.filter_by(
         id=variation_id,
         exercise_id=exercise_id
     ).first()
     validate_existence(variation, "variation")
-    
-    records = CalisthenicRecord.query.filter_by(
+
+    records = CardioRecord.query.filter_by(
         user_id=user_id,
         variation_id=variation_id
     ).all()
@@ -27,20 +27,21 @@ def get_calisthenic_variation_records(user_id, exercise_id, variation_id):
 
     return jsonify(all_records), 200
 
-@calisthenic_records_api.route('/users/<int:user_id>/exercises/calisthenic/<int:exercise_id>/variations/<int:variation_id>/records',
+@cardio_records_api.route('/users/<int:user_id>/exercises/cardio/<int:exercise_id>/variations/<int:variation_id>/records',
                                methods=["POST"])
-def add_calisthenic_variation_record(user_id, exercise_id, variation_id):
+def add_cardio_variation_record(user_id, exercise_id, variation_id):
     data = request.json
-    validate_existence(data.get("repetitions"), "repetitions")
+    validate_existence(data.get("calories"), "calories")
 
-    variation = CalisthenicExerciseVariations.query.filter_by(
+    variation= CardioRecord.query.filter_by(
         id=variation_id,
         exercise_id=exercise_id
     ).first()
     validate_existence(variation, "variation")
 
-    new_record = CalisthenicRecord(
-        repetitions=data["repetitions"],
+    new_record = CardioRecord(
+        calories=data["calories"],
+        time=data["time"],
         date = datetime.utcnow().date,
         is_a_challenge=data.get("is_a_challenge", False),
         user_id=user_id,
@@ -52,16 +53,16 @@ def add_calisthenic_variation_record(user_id, exercise_id, variation_id):
 
     return jsonify(new_record.serialize()), 201
 
-@calisthenic_records_api.route('/users/<int:user_id>/exercises/calisthenic/<int:exercise_id>/variations/<int:variation_id>/records/<int:record_id>',
+@cardio_records_api.route('/users/<int:user_id>/exercises/cardio/<int:exercise_id>/variations/<int:variation_id>/records/<int:record_id>',
                                methods=["GET"])
-def get_single_calisthenic_variation_record(user_id, exercise_id, variation_id, record_id):
-    variation = CalisthenicExerciseVariations.query.filter_by(
+def get_single_cardio_variation_record(user_id, exercise_id, variation_id, record_id):
+    variation = CardioExerciseVariations.query.filter_by(
         id=variation_id,
-        exercise_id=exercise_id 
+        exercise_id=exercise_id
     ).first()
     validate_existence(variation, "variation")
 
-    record = CalisthenicRecord.query.filter_by(
+    record = CardioRecord.query.filter_by(
         id=record_id,
         user_id=user_id,
         variation_id=variation_id
@@ -70,16 +71,16 @@ def get_single_calisthenic_variation_record(user_id, exercise_id, variation_id, 
 
     return jsonify(record.serialize()), 200
 
-@calisthenic_records_api.route('/users/<int:user_id>/exercises/calisthenic/<int:exercise_id>/variations/<int:variation_id>/records/<int:record_id>',
+@cardio_records_api.route('/users/<int:user_id>/exercises/cardio/<int:exercise_id>/variations/<int:variation_id>/records/<int:record_id>',
                                methods=["PUT"])
-def edit_single_calisthenic_variation_record(user_id, exercise_id, variation_id, record_id):
-    variation = CalisthenicExerciseVariations.query.filter_by(
+def edit_single_cardio_variation_record(user_id, exercise_id, variation_id, record_id):
+    variation = CardioExerciseVariations.query.filter_by(
         id=variation_id,
         exercise_id=exercise_id
     ).first()
     validate_existence(variation, "variation")
 
-    record = CalisthenicRecord.query.filter_by(
+    record = CardioRecord.query.filter_by(
         id=record_id,
         user_id=user_id,
         variation_id=variation_id
@@ -89,24 +90,24 @@ def edit_single_calisthenic_variation_record(user_id, exercise_id, variation_id,
     data = request.json
     validate_existence(data, "data")
 
-    record.repetitions = data["repetitions"]
+    record.calories = data["calories"]
     if "date" in data:
         record.date = datetime.strptime(data["date"], "%d/%m/%Y").date()
-
+    
     db.session.commit()
 
     return jsonify({'message': 'The record has been edited correctly', 'record': record.serialize()}), 200
 
-@calisthenic_records_api.route('/users/<int:user_id>/exercises/calisthenic/<int:exercise_id>/variations/<int:variation_id>/records/<int:record_id>',
+@cardio_records_api.route('/users/<int:user_id>/exercises/cardio/<int:exercise_id>/variations/<int:variation_id>/records/<int:record_id>',
                                methods=["DELETE"])
-def delete_single_calisthenic_variation_record(user_id, exercise_id, variation_id, record_id):
-    variation = CalisthenicExerciseVariations.query.filter_by(
+def delete_single_cardio_variation_record(user_id, exercise_id, variation_id, record_id):
+    variation = CardioExerciseVariations.query.filter_by(
         id=variation_id,
         exercise_id=exercise_id
     ).first()
     validate_existence(variation, "variation")
 
-    record = CalisthenicRecord.query.filter_by(
+    record = CardioRecord.query.filter_by(
         id=record_id,
         user_id=user_id,
         variation_id=variation_id
