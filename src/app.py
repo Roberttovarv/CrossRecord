f31739653eda9a8372449633b145ab8354ac88d7
Tovar <tovarobe@amazon.es>
2025-05-05 16:13:01 +0200

    Created variations folder and put variations routes inside. Also registered the blueprints in app.py

--- src/app.py
+++ src/app.py
@@ -3,15 +3,21 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 import os
-from .db.routes.auth_routes import auth_api
-from .db.routes.user_routes import user_api
-from .db.routes.calisthenic_variations_routes import calisthenic_variations_api
-from .db.routes.weighted_variations_routes import weighted_variations_api
-from .db.routes.cardio_variations_routes import cardio_variations_api
-from .db.routes.exercises_routes import exercises_api
-from .db.routes.follow_routes import follow_api
 from src.extensions import db
 from flask_jwt_extended import JWTManager
+
+
+from .db.routes.auth_routes import auth_api
+from .db.routes.user_routes import user_api
+from .db.routes.variations.calisthenic_variations_routes import calisthenic_variations_api
+from .db.routes.variations.weighted_variations_routes import weighted_variations_api
+from .db.routes.variations.cardio_variations_routes import cardio_variations_api
+from .db.routes.exercises_routes import exercises_api
+from .db.routes.follow_routes import follow_api
+from .db.routes.records.calisthenic_records_routes import calisthenic_records_api
+from .db.routes.records.cardio_records_routes import cardio_records_api
+from .db.routes.records.weighted_records_routes import weighted_records_api
+
 
 jwt = JWTManager()
 
@@ -39,6 +45,9 @@ def create_app():
     app.register_blueprint(cardio_variations_api)
     app.register_blueprint(exercises_api)
     app.register_blueprint(follow_api)
+    app.register_blueprint(cardio_records_api)
+    app.register_blueprint(calisthenic_records_api)
+    app.register_blueprint(weighted_records_api)
 
     from .db.models import user_models, calisthenic_models, cardio_models, weighted_models, challenge_models, user_follows_models
     from .db.routes import auth_routes, calisthenic_variations_routes, exercises_routes, user_routes, weighted_variations_routes
